10e745e2537a2b80f7fa249995503b32909a9842
femontanha <femontanhanew@gmail.com>
2018-10-28 01:34:01 -0200

    Updates

---
+++ precache-manifest.5f9db1ee67e8206269f920f4576980d5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6c8b4e8eb05187d7e4adc581108c377",
+    "url": "/fuud/static/media/spotifood.e6c8b4e8.svg"
+  },
+  {
+    "revision": "6f8f2d262ba47a9b5eceabc6f625f3c6",
+    "url": "/fuud/static/media/loading-inverted.6f8f2d26.svg"
+  },
+  {
+    "revision": "47494836ef47d6e92885d180867fb621",
+    "url": "/fuud/static/media/app-bg.47494836.png"
+  },
+  {
+    "revision": "b2e414809d6924fdc728fe5b3887ec9b",
+    "url": "/fuud/static/media/404.b2e41480.png"
+  },
+  {
+    "revision": "2201bb863314c08aa0c0",
+    "url": "/fuud/static/js/runtime~main.2201bb86.js"
+  },
+  {
+    "revision": "e8d7ee58dade4a366652",
+    "url": "/fuud/static/js/main.e8d7ee58.chunk.js"
+  },
+  {
+    "revision": "cb5347866c173fa3b997",
+    "url": "/fuud/static/js/1.cb534786.chunk.js"
+  },
+  {
+    "revision": "e8d7ee58dade4a366652",
+    "url": "/fuud/static/css/main.837049ba.chunk.css"
+  },
+  {
+    "revision": "cb5347866c173fa3b997",
+    "url": "/fuud/static/css/1.b63b2e4f.chunk.css"
+  },
+  {
+    "revision": "bbda042f55efabf77df8de1470a4d7a3",
+    "url": "/fuud/index.html"
+  }
+];
